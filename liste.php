67f39e3c70b2a42733a48623f85796ccb5a6107f
SamyOhr <69979570+SamyOhr@users.noreply.github.com>
2023-03-01 12:04:43 +0100

    push

--- liste.php
+++ liste.php
@@ -13,6 +13,47 @@ $retour = $base->query('SELECT * FROM movies;');
 while ($data = $retour->fetch()){
 echo $data['id']." ".$data['titre']." ".$data['genre']." ".$data['annee']."</br>";
 }
+<?php
+// Connexion à la base de données
+$host = "localhost";
+$user = "id20205701_samyouicher";
+$password = "/&*hX18M$A}2#QGr";
+$dbname = "id20205701_samy";
+$conn = mysqli_connect($host, $user, $password, $dbname);
+
+// Vérifier la connexion
+if (!$conn) {
+  die("La connexion à la base de données a échoué : " . mysqli_connect_error());
+}
+
+// Récupérer l'année de recherche
+$annee = $_GET['annee'];
+
+// Échapper les caractères spéciaux pour éviter les injections SQL
+$annee = mysqli_real_escape_string($conn, $annee);
+
+// Construire la requête SQL
+$sql = "SELECT titre FROM films WHERE annee = $annee";
+
+// Exécuter la requête SQL
+$result = mysqli_query($conn, $sql);
+
+// Vérifier si des résultats ont été trouvés
+if (mysqli_num_rows($result) > 0) {
+  // Afficher les résultats sous forme de liste
+  echo "<ul>";
+  while ($row = mysqli_fetch_assoc($result)) {
+    echo "<li>" . $row["titre"] . "</li>";
+  }
+  echo "</ul>";
+} else {
+  echo "Aucun film trouvé pour l'année $annee";
+}
+
+// Fermer la connexion à la base de données
+mysqli_close($conn);
+?>
+
 
 ?>
 
