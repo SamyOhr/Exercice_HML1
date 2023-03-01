20e49c113d1ba06a236cd71f8ce3237ee5f37560
SamyOhr <69979570+SamyOhr@users.noreply.github.com>
2023-03-01 12:50:20 +0100

    push

--- liste.php
+++ liste.php
@@ -1,33 +1,40 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Liste des films</title>
-</head>
-<body>
-	<h1>Liste des films de l'année <?php echo $_GET['ANNEE']; ?></h1>
-	<?php
-		// Connexion à la base de données
-		$base = new PDO('mysql:host=localhost; dbname=id20205701_samy', 'id20205701_samyouicher', '/&*hX18M$A}2#QGr');
-		$base->exec("SET CHARACTER SET utf8");
-		
-		// Requête pour récupérer les films de l'année sélectionnée
-		$retour = $base->prepare('SELECT * FROM movies WHERE annee = :ANNEE ORDER BY titre;');
-		$retour->execute(array('ANNEE' => $_GET['ANNEE']));
-		
-		// Vérification s'il y a des résultats
-		if ($retour->rowCount() > 0) {
-			echo '<ul>';
-			// Boucle pour afficher les résultats
-			while ($data = $retour->fetch()) {
-				echo '<li>' . $data['titre'] . ' (' . $data['annee'] . ')</li>';
-			}
-			echo '</ul>';
-		} else {
-			echo '<p>Aucun film disponible pour l\'année sélectionnée.</p>';
-		}
-		
-		// Fermeture de la connexion à la base de données
-		$base = null;
-	?>
-</body>
-</html
+<?php
+//Lister le contenu de la table movies
+//1° - Connexion à la BDD
+$base = new PDO('mysql:host=localhost; dbname=id20205701_samy', 'id20205701_samyouicher', '/&*hX18M$A}2#QGr');
+$base->exec("SET CHARACTER SET utf8");
+
+// Récupérer les années disponibles dans la base de données
+$annees = array();
+$retour = $base->query('SELECT DISTINCT annee FROM movies;');
+while ($data = $retour->fetch()){
+    $annees[] = $data['annee'];
+}
+
+// Récupérer l'année de recherche
+$ANNEE = isset($_GET['ANNEE']) ? $_GET['ANNEE'] : '';
+
+// Échapper les caractères spéciaux pour éviter les injections SQL
+$ANNEE = mysqli_real_escape_string($conn, $ANNEE);
+
+// Construire la requête SQL
+$sql = "SELECT titre FROM films WHERE annee = $ANNEE";
+
+// Exécuter la requête SQL
+$result = mysqli_query($conn, $sql);
+
+// Vérifier si des résultats ont été trouvés
+if (mysqli_num_rows($result) > 0) {
+  // Afficher les résultats sous forme de liste
+  echo "<ul>";
+  while ($row = mysqli_fetch_assoc($result)) {
+    echo "<li>" . $row["titre"] . "</li>";
+  }
+  echo "</ul>";
+} else {
+  echo "Aucun film trouvé pour l'année $ANNEE";
+}
+
+// Fermer la connexion à la base de données
+mysqli_close($conn);
+?>
