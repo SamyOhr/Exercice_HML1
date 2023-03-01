28a747a7b94341a5f15968aba3412a79ced5062d
SamyOhr <69979570+SamyOhr@users.noreply.github.com>
2023-03-01 11:47:39 +0100

    push

--- liste.php
+++ liste.php
@@ -6,7 +6,6 @@
 //1° - Connexion à la BDD
 $base = new PDO('mysql:host=localhost; dbname=id20205701_samy', 'id20205701_samyouicher', '/&*hX18M$A}2#QGr');
 $base->exec("SET CHARACTER SET utf8");
-echo 'coucou';
 //2° - Préparation de requête et exécution
 $retour = $base->query('SELECT * FROM movies;');
 
