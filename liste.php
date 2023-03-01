36169c1d1daaff902380cef0e39e21d7162334b9
SamyOhr <69979570+SamyOhr@users.noreply.github.com>
2023-03-01 12:09:46 +0100

    PUSH

--- liste.php
+++ liste.php
@@ -15,13 +15,13 @@ echo $data['id']." ".$data['titre']." ".$data['genre']." ".$data['annee']."</br>
 }
 
 // Récupérer l'année de recherche
-$annee = $_GET['annee'];
+$ANNEE = $_GET['annee'];
 
 // Échapper les caractères spéciaux pour éviter les injections SQL
-$annee = mysqli_real_escape_string($conn, $annee);
+$ANNEE = mysqli_real_escape_string($conn, $ANNEE);
 
 // Construire la requête SQL
-$sql = "SELECT titre FROM films WHERE annee = $annee";
+$sql = "SELECT titre FROM films WHERE annee = $ANNEE";
 
 // Exécuter la requête SQL
 $result = mysqli_query($conn, $sql);
@@ -35,7 +35,7 @@ if (mysqli_num_rows($result) > 0) {
   }
   echo "</ul>";
 } else {
-  echo "Aucun film trouvé pour l'année $annee";
+  echo "Aucun film trouvé pour l'année $ANNEE";
 }
 
 // Fermer la connexion à la base de données
