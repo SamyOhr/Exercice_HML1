7712a0788afb96efc17b234498db77f4c67491c0
SamyOhr <69979570+SamyOhr@users.noreply.github.com>
2023-03-01 13:06:58 +0100

    push

--- liste.php
+++ liste.php
@@ -1,18 +1,18 @@
-<p>ANNEE ? <SELECT name="annee">
 <?php
-//Lister le contenu de la table movies
-//1° - Connexion à la BDD
+// Récupérer l'année sélectionnée dans le menu déroulant
+$annee = $_GET['annee'];
+
+// Connexion à la BDD
 $base = new PDO('mysql:host=localhost; dbname=id20205701_samy', 'id20205701_samyouicher', '/&*hX18M$A}2#QGr');
 $base->exec("SET CHARACTER SET utf8");
 
-//2° - Préparation de requette et execution
-$retour = $base->query('SELECT DISTINCT annee FROM movies;');
+// Préparation de la requête et exécution
+$requete = $base->prepare('SELECT * FROM movies WHERE annee = :annee');
+$requete->bindParam(':annee', $annee);
+$requete->execute();
 
-//3° - Lecture du resultat de la requette
-while ($data = $retour->fetch()){
-echo "<option>".$data['annee']."</option>";
+// Affichage des résultats
+while ($data = $requete->fetch()){
+	echo $data['titre'].' - '.$data['realisateur'].' - '.$data['annee'].'<br>';
 }
-
 ?>
-</SELECT>
-</p>
