5ea15c2ea072b833b9b09b75f778eb8618b7621b
SamyOhr <69979570+SamyOhr@users.noreply.github.com>
2023-03-01 12:27:20 +0100

    push

---
+++ liste1.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+<h1>Liste des films</h1>
+
+
+<?php
+//Lister le contenu de la table movies
+
+//1° - Connexion à la BDD
+$base = new PDO('mysql:host=localhost; dbname=id20205701_samy', 'id20205701_samyouicher', '/&*hX18M$A}2#QGr');
+$base->exec("SET CHARACTER SET utf8");
+
+//2° - Préparation de requette et execution
+$retour = $base->query('SELECT * FROM movies;');
+
+//3° - Lecture du resultat de la requette
+while ($data = $retour->fetch()){
+echo $data['id']." ".$data['titre']." ".$data['genre']." ".$data['annee']."</br>";
+}
+
+?>
+
+</body>
+</html>
