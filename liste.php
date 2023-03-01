c97b1c262e165999821bd1300a9cce1cf6188345
SamyOhr <69979570+SamyOhr@users.noreply.github.com>
2023-03-01 14:13:21 +0100

    push

--- liste.php
+++ liste.php
@@ -1,6 +1,8 @@
 <p>ANNEE ? <SELECT name="annee">
 
 <?php
+$base = new PDO('mysql:host=localhost; dbname=id20205701_samy', 'id20205701_samyouicher', '/&*hX18M$A}2#QGr');
+$base->exec("SET CHARACTER SET utf8");
 
 $retour = $base->query('SELECT DISTINCT annee FROM movies;');
 
