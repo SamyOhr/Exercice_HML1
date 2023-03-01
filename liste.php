d326b4c57b5eb4d759c767b35935ee2dc472ae22
SamyOhr <69979570+SamyOhr@users.noreply.github.com>
2023-03-01 12:35:12 +0100

    push

--- liste.php
+++ liste.php
@@ -1,39 +1,33 @@
 <!DOCTYPE html>
 <html>
+<head>
+	<title>Liste des films</title>
+</head>
 <body>
-<h1>Liste des films</h1>
-<?php
-// Connexion à la BDD avec PDO
-$base = new PDO('mysql:host=localhost; dbname=id20205701_samy', 'id20205701_samyouicher', '/&*hX18M$A}2#QGr');
-$base->exec("SET CHARACTER SET utf8");
-
-// Vérifier si l'année a été spécifiée dans la requête GET
-if (isset($_GET['ANNEE'])) {
-    $ANNEE = $_GET['ANNEE'];
-    
-    // Se connecter à la base de données
-    $base = new PDO('mysql:host=localhost; dbname=id20205701_samy', 'id20205701_samyouicher', '/&*hX18M$A}2#QGr');
-    $base->exec("SET CHARACTER SET utf8");
-    
-    // Échapper les caractères spéciaux pour éviter les injections SQL
-    $ANNEE = $base->quote($ANNEE);
-    
-    // Récupérer la liste des films de l'année spécifiée
-    $films = $base->query("SELECT titre FROM movies WHERE annee = $ANNEE;");
-    
-    // Afficher la liste des films
-    if ($films->rowCount() > 0) {
-        echo "<ul>";
-        while ($film = $films->fetch()) {
-            echo "<li>" . $film['titre'] . "</li>";
-        }
-        echo "</ul>";
-    } else {
-        echo "Aucun film trouvé pour l'année $ANNEE";
-
-
-// Fermer la connexion à la base de données
-$base = null;
-?>
+	<h1>Liste des films de l'année <?php echo $_GET['ANNEE']; ?></h1>
+	<?php
+		// Connexion à la base de données
+		$base = new PDO('mysql:host=localhost; dbname=id20205701_samy', 'id20205701_samyouicher', '/&*hX18M$A}2#QGr');
+		$base->exec("SET CHARACTER SET utf8");
+		
+		// Requête pour récupérer les films de l'année sélectionnée
+		$retour = $base->prepare('SELECT * FROM movies WHERE annee = :ANNEE ORDER BY titre;');
+		$retour->execute(array('ANNEE' => $_GET['ANNEE']));
+		
+		// Vérification s'il y a des résultats
+		if ($retour->rowCount() > 0) {
+			echo '<ul>';
+			// Boucle pour afficher les résultats
+			while ($data = $retour->fetch()) {
+				echo '<li>' . $data['titre'] . ' (' . $data['annee'] . ')</li>';
+			}
+			echo '</ul>';
+		} else {
+			echo '<p>Aucun film disponible pour l\'année sélectionnée.</p>';
+		}
+		
+		// Fermeture de la connexion à la base de données
+		$base = null;
+	?>
 </body>
-</html>
+</html
