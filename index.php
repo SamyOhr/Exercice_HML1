4e1d0276df61b806012377e82ee86e8cb0cfda65
SamyOhr <69979570+SamyOhr@users.noreply.github.com>
2023-03-20 10:02:37 +0100

    push

--- index.php
+++ index.php
@@ -1,61 +1,37 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Jeux Javascript</title>
-    <meta charset="utf-8">
+    <title>Jeu JavaScript</title>
     <style type="text/css">
         #zonedejeux {
-            width: 900px;
-            height: 400px;
             border: 1px solid black;
         }
     </style>
 </head>
 <body onload="principale()">
-    <h1>Jeux Javascript</h1>
+    <h1>Jeu JavaScript</h1>
     <svg id="zonedejeux">
-        <circle cx="450" cy="200" r="15" fill="tomato" id="player" />
+        <circle cx="50" cy="200" r="15" fill="red" id="player" />
         <circle cx="800" cy="200" r="10" fill="blue" id="enemy" />
     </svg>
     <script type="text/javascript">
-        var playerY = 200;   // Initial position of the player circle
-        var enemyX = 800;    // Initial position of the enemy circle
-        var speed = 50;      // Speed of enemy movement (ms)
+        var playerY = 200;
+        var ballY = 200;
+        var enemyX = 800;
+        var speed = 50;
 
-        function principale() {
-            // Add listener to arrow keys
-            document.addEventListener('keydown', deplacement);
-
-            // Start moving the enemy ball
+        function principale(){
+            document.onkeydown = deplacement;
             setInterval(bouger, speed);
         }
 
-        function deplacement(evt) {
-            var code = evt.keyCode;
-            var ballY = playerY;
-
-            switch (code) {
-                // Right
-                case 39:
-                    var ballX = document.getElementById('player').getAttribute('cx');
-                    ballX = parseInt(ballX) + 15;
-                    if (ballX > 885) {ballX = 885;}
-                    document.getElementById('player').setAttribute("cx", ballX);
-                    break;
-
+        function deplacement(evt){
+            switch(evt.keyCode){
                 // Up
                 case 38:
                     ballY -= 15;
                     if (ballY < 15){ballY = 15;}
                     document.getElementById('player').setAttribute("cy", ballY);
-                    break;
-
-                // Left
-                case 37:
-                    var ballX = document.getElementById('player').getAttribute('cx');
-                    ballX = parseInt(ballX) - 15;
-                    if (ballX < 15) {ballX = 15;}
-                    document.getElementById('player').setAttribute("cx", ballX);
                     break;
 
                 // Down
